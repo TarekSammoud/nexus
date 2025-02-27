74b5d3dfc6d9db32cb173820269c50b59ef0ec32
tareksammoud <tarek.sammoud@esprit.tn>
2025-02-27 21:23:53 +0100

    Project creation : backend intellij available, frontend angular available

---
+++ nexus-backend/src/test/java/tn/arctic/nexus/NexusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.arctic.nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
