0b0846659d25352c0167ba712545b96afc703086
wjbwastaken1 <99321808+wjbwastaken1@users.noreply.github.com>
2025-03-12 17:54:13 +0100

    some fixes

--- nexus-backend/src/main/java/tn/arctic/nexus/entities/Bid.java
+++ nexus-backend/src/main/java/tn/arctic/nexus/entities/Bid.java
@@ -1,5 +1,7 @@
 package tn.arctic.nexus.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,7 +15,9 @@ import java.util.List;
     @Data
     @AllArgsConstructor
     @NoArgsConstructor
+
     public class Bid {
+
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
@@ -24,12 +28,62 @@ import java.util.List;
 
         private Double amount;
         private LocalDateTime createdAt;
+
+        @JsonProperty("isWinningBid")
         private Boolean isWinningBid;
 
         @ManyToOne
         @JoinColumn(name = "market_listing_id", referencedColumnName = "id", nullable = false)
+        @JsonIgnore
         private MarketListing marketListing;
 
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public User getUser() {
+            return user;
+        }
+
+        public void setUser(User user) {
+            this.user = user;
+        }
+
+        public Double getAmount() {
+            return amount;
+        }
+
+        public void setAmount(Double amount) {
+            this.amount = amount;
+        }
+
+        public LocalDateTime getCreatedAt() {
+            return createdAt;
+        }
+
+        public void setCreatedAt(LocalDateTime createdAt) {
+            this.createdAt = createdAt;
+        }
+
+        public Boolean getWinningBid() {
+            return isWinningBid;
+        }
+
+        public void setWinningBid(Boolean winningBid) {
+            isWinningBid = winningBid;
+        }
+
+        public MarketListing getMarketListing() {
+            return marketListing;
+        }
+
+        public void setMarketListing(MarketListing marketListing) {
+            this.marketListing = marketListing;
+        }
     }
 
 
