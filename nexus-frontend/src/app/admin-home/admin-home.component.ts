a38f2b823948e2c3e810ba3fcacf8f1b51b5131b
tareksammoud <tarek.sammoud@esprit.tn>
2025-04-05 16:08:33 +0100

    implementing backoffice

--- nexus-frontend/src/app/admin-home/admin-home.component.ts
+++ nexus-frontend/src/app/admin-home/admin-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-home',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-home.component.css']
 })
 export class AdminHomeComponent {
+  constructor(private _router: Router) { }
+  dropdownOpen = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  navigateToGamesList(){
+    this._router.navigate([{ outlets: { modal: 'admin/games/list' } }]);
+  }
+
+  isCollapsed = false;  // Track whether sidebar is collapsed
+
+  // Toggle the sidebar state
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+
 
 }
