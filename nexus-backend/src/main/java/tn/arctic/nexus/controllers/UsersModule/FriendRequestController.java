ff816ea0f5375dd84e409667d5acb534b4443f7a
aziz-benzekri <mohamedaziz.benzekri@esprit.tn>
2025-04-07 15:54:21 +0100

    recuperation des user qui ne sont pas amis

--- nexus-backend/src/main/java/tn/arctic/nexus/controllers/UsersModule/FriendRequestController.java
+++ nexus-backend/src/main/java/tn/arctic/nexus/controllers/UsersModule/FriendRequestController.java
@@ -2,16 +2,21 @@ package tn.arctic.nexus.controllers.UsersModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import tn.arctic.nexus.entities.FriendRequest;
+import tn.arctic.nexus.entities.User;
+import tn.arctic.nexus.services.UsersModule.FriendRequestService;
 import tn.arctic.nexus.services.UsersModule.IFriendRequestService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/friendRequests")
 public class FriendRequestController {
 
     @Autowired
     private IFriendRequestService friendRequestService;
+
+
 
     @GetMapping("/allFriendRequest")
     public List<FriendRequest> getAllFriendRequests() {
@@ -37,4 +42,14 @@ public class FriendRequestController {
     public void deleteFriendRequest(@PathVariable Long id) {
         friendRequestService.removeFriendRequest(id);
     }
+
+
+    // pour retourner les user qui n ont pas des invitavtion
+    @GetMapping("/players/available/{id}")
+    public List<User> getAvailablePlayers(@PathVariable Long id) {
+        System.out.println("==> getAvailablePlayers called for id: " + id);
+        return friendRequestService.findAvailablePlayersForFriendRequest(id);
+    }
+
+
 }
