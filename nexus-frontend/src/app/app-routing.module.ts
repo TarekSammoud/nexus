39699c99a74f4fde58cba081e0f6b25b1cc50107
tareksammoud <tarek.sammoud@esprit.tn>
2025-04-15 17:14:59 +0100

    Revert "Merge remote-tracking branch 'origin/GameJamsManagement' into GameManagement"
    This reverts commit 5cd957390c49bfbb20a4410c04b0c4a7674c2255, reversing
    changes made to 83bb7c66471f8adc0f1edc38ba47fa67943c8f30.

--- nexus-frontend/src/app/app-routing.module.ts
+++ nexus-frontend/src/app/app-routing.module.ts
@@ -36,7 +36,6 @@ const routes: Routes = [
   {path: 'category/:name', component: GameGridComponent},
   {path: 'admin', component: AdminHomeComponent},
   {path: 'admin/categories', component: GameCategoriesComponent},
-  {path: 'admin/games/list', component: CreateGameComponent, outlet: 'adminOutlet'},
 
 ];
 
