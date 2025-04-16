b98899f9cfd4d9bd87d47942bb213dc2b748a261
tareksammoud <tarek.sammoud@esprit.tn>
2025-04-16 23:07:51 +0100

    fixed dependencies and home game catalogue

--- nexus-frontend/src/app/games/game-grid/game-grid.component.ts
+++ nexus-frontend/src/app/games/game-grid/game-grid.component.ts
@@ -17,23 +17,42 @@ export class GameGridComponent {
   constructor(private _router:Router,private _gameService: GameService,private route: ActivatedRoute) {
     // Initialize the component
     const category = this.route.snapshot.paramMap.get('name');
-    const formattedCategory = category ? category.replace(/ /g, '_') : ''; // Handle null case
-        this.name = category!;
+    if (category) {
+      const formattedCategory = category ? category.replace(/ /g, '_') : ''; // Handle null case
+      this.name = category!;
 
-    this._gameService.getGamesByCategory(formattedCategory!).subscribe(games => {
-      this.games = games;
-      for (let i = 0; i < this.games.length; i++) {
-        for (let j = 0; j < this.games[i].gameMediaList.length; j++) {
-          if (this.games[i].gameMediaList[j].gameMediaType == 'COVER') {
-            this.games[i].coverPicture = this.games[i].gameMediaList[j]; 
-            console.log(this.games[i].coverPicture?.mediaUrl);
-            break; 
-          }
+  this._gameService.getGamesByCategory(formattedCategory!).subscribe(games => {
+    this.games = games;
+    for (let i = 0; i < this.games.length; i++) {
+      for (let j = 0; j < this.games[i].gameMediaList.length; j++) {
+        if (this.games[i].gameMediaList[j].gameMediaType == 'COVER') {
+          this.games[i].coverPicture = this.games[i].gameMediaList[j]; 
           console.log(this.games[i].coverPicture?.mediaUrl);
+          break; 
         }
+        console.log(this.games[i].coverPicture?.mediaUrl);
       }
-      console.log(this.games);
-    })
+    }
+    console.log(this.games);
+  })
+    }
+    else {
+      this._gameService.getGames().subscribe(games => {
+        this.games = games;
+        for (let i = 0; i < this.games.length; i++) {
+          for (let j = 0; j < this.games[i].gameMediaList.length; j++) {
+            if (this.games[i].gameMediaList[j].gameMediaType == 'COVER') {
+              this.games[i].coverPicture = this.games[i].gameMediaList[j]; 
+              console.log(this.games[i].coverPicture?.mediaUrl);
+              break; 
+            }
+            console.log(this.games[i].coverPicture?.mediaUrl);
+          }
+        }
+        console.log(this.games);
+      })
+    }
+  
   }
 
     
