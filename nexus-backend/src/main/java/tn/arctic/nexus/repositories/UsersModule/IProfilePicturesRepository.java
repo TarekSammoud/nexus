4dc13a3953282e559785d0bd8fa0bf97cbf925ef
aziz-benzekri <mohamedaziz.benzekri@esprit.tn>
2025-04-28 23:35:57 +0100

    .

--- nexus-backend/src/main/java/tn/arctic/nexus/repositories/UsersModule/IProfilePicturesRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.arctic.nexus.repositories.UsersModule;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.arctic.nexus.entities.ProfilePictures;
-
-public interface IProfilePicturesRepository extends JpaRepository<ProfilePictures,Long> {
-    ProfilePictures findByUserId(Long userId);
-
-}
--- nexus-backend/src/main/java/tn/arctic/nexus/repositories/UsersModule/IUsersModuleUserVerificationRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.arctic.nexus.repositories.UsersModule;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.arctic.nexus.entities.UserVerification;
-
-public interface IUsersModuleUserVerificationRepository extends JpaRepository<UserVerification, Long> {
-    UserVerification findByEmail(String email);
-}
