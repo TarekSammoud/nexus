4034ed18484d897798e847fc51fe7fd3d400b936
TarekSammoud <tarek.sammoud@esprit.tn>
2025-05-05 10:44:20 +0000

    added prometheus and grafana monitoring

---
+++ deployment_files/prometheus+grafana/flask_exporter/flask_exporter.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask, request
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+
+# Get the metrics endpoint from environment variables (configured by Kubernetes)
+metrics_endpoint = os.getenv("FLASK_METRICS_ENDPOINT", "/metrics")
+app_endpoint = os.getenv("FLASK_APP_ENDPOINT", "http://spam-ai.ai.svc.cluster.local:80")
+
+metrics = PrometheusMetrics(app)
+
+# static information as metric
+metrics.info('app_info', 'Application info', version='1.0.3')
+
+@app.route('/')
+def main():
+    pass  # requests tracked by default
+
+@app.route('/skip')
+@metrics.do_not_track()
+def skip():
+    pass  # default metrics are not collected
+
+@app.route('/<item_type>')
+@metrics.do_not_track()
+@metrics.counter('invocation_by_type', 'Number of invocations by type',
+         labels={'item_type': lambda: request.view_args['item_type']})
+def by_type(item_type):
+    pass  # only the counter is collected, not the default metrics
+
+@app.route('/long-running')
+@metrics.gauge('in_progress', 'Long running requests in progress')
+def long_running():
+    pass
+
+@app.route('/status/<int:status>')
+@metrics.do_not_track()
+@metrics.summary('requests_by_status', 'Request latencies by status',
+                 labels={'status': lambda r: r.status_code})
+@metrics.histogram('requests_by_status_and_path', 'Request latencies by status and path',
+                   labels={'status': lambda r: r.status_code, 'path': lambda: request.path})
+def echo_status(status):
+    return 'Status: %s' % status, status
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
